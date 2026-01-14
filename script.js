7ac14302fc96dedc5a581914fa0c805deb083ada
Thimo <i486459@fhict.nl>
2026-01-14 14:27:10 +0100

    deleted old files

--- script.js
+++
@@ -1,9 +0,0 @@
-// This file is currently very small
-// It can be expanded later with animations, interactions, or logic
-
-console.log("PaceSense website loaded");
-
-// Example future ideas:
-// - Scroll-based animations
-// - Button click handlers
-// - Theme toggles
